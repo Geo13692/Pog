aa3a2c7f1bc29257812982baa31c4903a13cc936
Geo29631 <geo13692roblox@gmail.com>
2020-11-26 18:05:02 -0600

    Gives pavalix pain.

---
+++ Java/Gun.java
@@ -0,0 +1,11 @@
+public class Gun {
+    String name;
+    int magcapacity;
+    String shoot(String Target){
+        return "pain";
+    }
+    Gun(String providedname){
+        this.name=providedname;
+        this.magcapacity=30;
+    }
+}
---
+++ Java/Main.java
@@ -0,0 +1,11 @@
+class Main {
+        public static void main(String[] args){
+                int Big=1;
+                boolean SkinnyGuy=true;
+                float JakeB=33/12734f;
+                double BigJake=12/6.6;
+                double fiveasanintger=(double)5;
+                Gun AR15=new Gun("AR15");
+                System.out.println(Boolean.toString(SkinnyGuy)+Integer.toString(Big)+Float.toString(JakeB)+Double.toString(BigJake)+AR15.shoot("Pavalix"));    
+        }
+}

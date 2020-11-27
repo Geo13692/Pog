35a0255ec72dae237c44c861db6431b3dea5591e
Geo29631 <geo13692roblox@gmail.com>
2020-11-26 19:42:43 -0600

    F

--- Java/Main.java
+++ Java/Main.java
@@ -1,11 +1,29 @@
 class Main {
+        // n > m
+        public static int euclid(int n, int m) {
+                int lastN = 0;
+                int nMinusM = n-m;
+                while (m !=0){
+                        int nMinusm = n-m;
+                        if(nMinusM > m) {
+                                m=m;
+                                lastN = n;
+                                n = nMinusm;}
+                        else{
+                                m=nMinusM;
+                                lastN = n;
+                                n=m;
+                            }
+                        }
+                        if (n==0){
+                        return n;
+                }
+                return m;
+        }
         public static void main(String[] args){
-                int Big=1;
-                boolean SkinnyGuy=true;
-                float JakeB=33/12734f;
-                double BigJake=12/6.6;
-                double fiveasanintger=(double)5;
-                Gun AR15=new Gun("AR15");
-                System.out.println(Boolean.toString(SkinnyGuy)+Integer.toString(Big)+Float.toString(JakeB)+Double.toString(BigJake)+AR15.shoot("Pavalix"));    
+                int greater, lesser;
+                greater = 25;
+                lesser = 15;
+                System.out.println(Integer.toString(euclid(greater, lesser)));
         }
 }
